c9a74354ef35543d74bc2d61422b94b20da6143b
Matthew Brown <mbrown@geomarvel.com>
2021-07-10 19:22:09 -0400

    cors fix

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const cors = require("cors");
 const planetsRouter = require("./routes/planets/planets.router");
 
 const app = express();
 
 // parses incoming json
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
 app.use(planetsRouter);
 
