061e79185553673d7f8008ced71ea12ecd466b6a
Matthew Brown <mbrown@geomarvel.com>
2021-07-10 19:03:31 -0400

    seperated out app and server

--- server/src/server.js
+++ server/src/server.js
@@ -1,1 +1,11 @@
-// npm init
+const http = require("http");
+const app = require("./app");
+
+// FE on port 3000 ---- checks env port or defaults to 8000
+const PORT = process.env.PORT || 8000;
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log("listening on port: ", PORT);
+});
