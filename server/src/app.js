57cb53edc89e1d97e756778041d27b5bcaeaad6a
Matthew Brown <mbrown@geomarvel.com>
2021-07-10 19:15:40 -0400

    get planets

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,11 @@
 const express = require("express");
+const planetsRouter = require("./routes/planets/planets.router");
 
 const app = express();
+
+// parses incoming json
+app.use(express.json());
+
+app.use(planetsRouter);
 
 module.exports = app;
