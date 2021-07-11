3f58ed33073be618fda0432c8b33f156fee3fc39
Matthew Brown <mbrown@geomarvel.com>
2021-07-11 19:03:57 -0400

    morgan added

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
+const morgan = require("morgan");
+
 const planetsRouter = require("./routes/planets/planets.router");
 
 const app = express();
@@ -12,6 +14,8 @@ app.use(
     origin: "http://localhost:3000",
   })
 );
+
+app.use(morgan("combined"));
 
 app.use(express.static(path.join(__dirname, "..", "public")));
 
