58308da648e776a565057a1509bf59c2de1f9382
Matthew Brown <mbrown@geomarvel.com>
2021-07-12 20:34:52 -0400

    connecting post launches to FE

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,8 +1,4 @@
-const {
-  launches,
-  getAllLaunches,
-  addNewLaunch,
-} = require("../../models/launches.model");
+const { getAllLaunches, addNewLaunch } = require("../../models/launches.model");
 
 function httpGetAllLaunches(req, res) {
   return res.status(200).json(getAllLaunches());
@@ -10,25 +6,30 @@ function httpGetAllLaunches(req, res) {
 
 function httpAddNewLaunch(req, res) {
   const launch = req.body;
+
   if (
     !launch.mission ||
     !launch.rocket ||
     !launch.launchDate ||
-    !launch.destination
+    !launch.target
   ) {
-    res.status(400).json({
+    return res.status(400).json({
       error: "Missing required launch property",
     });
   }
 
-  if (NaN(launch.launchDate)) {
+  launch.launchDate = new Date(launch.launchDate);
+  if (isNaN(launch.launchDate)) {
     return res.status(400).json({
       error: "Invalid launch date",
     });
   }
 
-  launch.launchDate.toString() === "Invalid date";
   addNewLaunch(launch);
   return res.status(201).json(launch);
 }
-module.exports = { httpGetAllLaunches, httpAddNewLaunch };
+
+module.exports = {
+  httpGetAllLaunches,
+  httpAddNewLaunch,
+};
